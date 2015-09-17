003ba6af75f70ad34c0b201150ec83ad122ca0fd
joshR2 <josh2cool123@gmail.com>
2015-09-17 09:07:46 -0600

    Added a user based monster.
    create monster a monster based on user input. practice using variables
    and scanner method(s).

--- src/marshmellows/controller/MonsterController.java
+++ src/marshmellows/controller/MonsterController.java
@@ -1,18 +1,18 @@
 package marshmellows.controller;
-import marshmellows.view.MarshmellowOutput;
-import marshmellows.model.MarshmellowMonster;
+
+
+
 import java.util.Scanner;
+
+import marshmellows.model.MarshmellowMonster;
+import marshmellows.view.MarshmellowOutput;
+
 public class MonsterController
 {
 	private MarshmellowMonster joshMonster;
 	private MarshmellowOutput myOutput;
-	
-	Public class MonsterController
-	{
-	private MarshmellowMonster codyMonster;
-	private MArshmellowOutpuy myOutput;
-	private Scanner mosnterScanner;
-	
+	private Scanner monsterScanner;
+	private MarshmellowMosnter userMonster;
 	
 	public MonsterController()
 	{
@@ -30,23 +30,59 @@ public class MonsterController
 	
 	public void start()
 	{
-		myOutput.displaMonsterInfo(codyMonster.toString());
+		myOutput.displayMonsterInfo(joshMonster.toString());
 		askQuestions();
 		myOutput.displayMonsterInfo(joshMonster.toString());
 	
 	}
-}
+
 
 
 	private void askQuestions()
 	{
-		System.out.println("Type in a better name for the monster";
+		System.out.println("Type in a better name for the monster");
 		String betterMonsterName = monsterScanner.next();
-		codyMonster.setMonsterName(betterMonsterName);
+		joshMonster.setMonsterName(betterMonsterName);
 		System.out.println("Type in a diffrent number of eyes fo r the mosnter");
 		int betterMonsterEyes = monsterScanner.nextInt();
-		codyMonster.setMonsterEyes(betterMonsterEyes);
+		joshMonster.setMonstername
+		betterEyes(betterMonsterEyes);
+		System.out.println("Type in a better name for the monster");
+		double betterMonsterLegs= monsterScanner.nextDouble();
 	}
 	
-}
+
+
+
+/**
+ * This method will get the information to create an instance of a MarshmellowMonster
+ */
+ private void makeUserMonster()
+ {
+	 //Step one: Get variables
+	 String userName; // Declare a string type called userName.
+	 int userEyes;    // Declare an int type called userEyes
+	 int userNoseCount; 
+	 double userHair;
+	 double userLegs;
+	 boolean userBellyButton;
+	 
+	 //Step two: Define variables by using scanner to get user input.
+	 System.out.println("Type in your name for your monster.");
+	 userName = monsterScanner.nextLine();
+	 System.out.println("Type in the number of eyes for your monster");
+	 userEyes = monsterScanner.nextInt();
+	 System.out.println("Type in the number of noses for this monster");
+	 userNoseCount = monsterScanner.nextInt();
+	 System.out.println("How much hair does your monster have? Type in a floating point or decimal number.");
+	 userHair = monsterScanner.nextDouble();
+	 System.out.println("How many legs??????");
+	 userLegs = monsterScanner.nextDouble();
+	 System.out.println("Does it have a bellybutton? Type true or false");
+	 userBellyButton = monsterScanner.nextBoolean();
+	 // Step three: Make a monster - use the Constructor! remember that the order of parameteres matters!
+	 userMonster = new MarshmellowMonster(userName, userEyes, userNoseCount, userHair, userLegs, userBellyButton);
+	 
+ } 
+ }
 	
